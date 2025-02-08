9c053110a009a2a779d66fdc4126b8d41837218d
Nyiriek <n.peat@alustudent.com>
2025-02-08 13:47:09 +0200

    Add files

--- react_intro/task_0/dashboard/src/App.js
+++ react_intro/task_0/dashboard/src/App.js
@@ -1,21 +1,29 @@
-import logo from './Holberton.png';
+import React from 'react';
 import './App.css';
+import logo from './Holberton-logo.jpg';
 
-function App() {
-  return (
-    <>
-    <div className="App-header">
-      <img src={logo} alt="Holberton Logo: Red Seahorse"/>
-      <h1>School dashboard</h1>
-    </div>
-      <div className="App-body">
-      <p>Login to access the full dashboard</p>
-    </div>
-      <div className="App-footer">
-      <p>Copyright 2020 - holberton School</p>
-    </div>
-    </>
-  );
+
+const App =() => {
+    return (
+        <div>
+            <div className="App-header">
+                
+                <img src={logo} alt="Logo" />
+                <h1>School dashboard</h1>
+                
+            </div>
+            <hr style={{ borderTop: '3px solid red', marginTop: '1px'}}/>
+
+            <div className="App-body">
+                <p>Login to access the full dashboard</p>
+            </div>
+            <hr style={{ borderTop: '3px solid red', marginTop: '500px'}}/>
+
+            <div className="App-footer">
+                <p>Copyright 2020 - holberton School</p>
+            </div>
+        </div>
+    );
 }
 
-export default App;
+export default App
--- react_intro/task_3/dashboard/src/App.js
+++ react_intro/task_3/dashboard/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import logo from './Holberton.png';
+import logo from './Holberton-logo.jpg';
 import { getFooterCopy, getFullYear } from './utils';
 
 
